34c964cd1f5e38b7d60486ff62b5af3616d9bc23
DNLMSANTOS <55152205+DNLMSANTOS@users.noreply.github.com>
2022-07-19 23:32:15 -0300

    add função de soma

---
+++ soma.py
@@ -0,0 +1,5 @@
+def adicao(x, y):
+    return (x + y)
+
+result = adicao(4,6)    
+print(result) 
---
+++ soma_test.py
@@ -0,0 +1,6 @@
+import pytest
+from pytest_mock import mocker
+from soma import adicao
+
+def test_adicao():
+    assert adicao(5,5) == 10
